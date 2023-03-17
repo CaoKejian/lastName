228f20a01ca6465a9f002c145d616c473af01b55
曹珂俭 <1849201815@qq.com>
2023-03-17 15:07:29 +0800

    创建validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
- namespace :api do
-  namespace :v1 do
-    resources :validation_codes,only: [:create]
-    resource :session, only: [:create, :destory]
-    resource :me, only: [:show]
-    resources :items
-    resources :tags
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destory]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
   end
- end
 end
