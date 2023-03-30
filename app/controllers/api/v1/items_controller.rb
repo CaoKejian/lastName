a5549699318dac3b864af87981aae8312868a3a8
曹珂俭 <1849201815@qq.com>
2023-03-30 22:40:41 +0800

    准备解决bug

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.where({created_at: params[:created_after]..  params[:created_before]}).page(params[:page])
+  
+    items = Item
+      .where({ created_at: params[:created_after]..params[:created_before] })
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
