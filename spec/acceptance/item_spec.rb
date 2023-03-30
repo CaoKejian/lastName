6b4f3cee50be52c3ffa9087c593b1362f75b8882
曹珂俭 <1849201815@qq.com>
2023-03-30 19:57:48 +0800

    完善 api/v1/items 文档

--- spec/acceptance/item_spec.rb
+++ spec/acceptance/item_spec.rb
@@ -3,7 +3,6 @@ require 'rspec_api_documentation/dsl'
 
 resource "账目" do
   get "/api/v1/items" do
-    # authentication :basic, :auth
     parameter :page, '页码'
     parameter :created_after, '创建时间起点（筛选条件）' 
     parameter :created_before, '创建时间终点（筛选条件）' 
@@ -13,12 +12,8 @@ resource "账目" do
     end
     let(:created_after) { '2020-10-10'}
     let(:created_before) { '2020-11-11'}
-    # let(:current_user) { User.create email: '1@qq.com' }
-    # let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "获取账目" do
-      11.times do Item.create amount: 100, created_at: '2020-10-30' 
-        
-      end
+      11.times do Item.create amount: 100, created_at: '2020-10-30' end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
