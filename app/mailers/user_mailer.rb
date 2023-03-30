df85c58c1a6dd406383797920d6c7595e6e003dc
曹珂俭 <1849201815@qq.com>
2023-03-30 10:42:42 +0800

    测试邮件被成功发送

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email
-    mail(to: "1849201815@qq.com", subject: "Welcome to My Awesome Site")
+  def welcome_email(code)
+    validation_code = ValidationCode.find_by_email(email)
+    @code = validation_code
+    mail(to: email, subject: "【山竹记账】验证码：")
   end
 end
--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -3,11 +3,10 @@ require "rspec_api_documentation/dsl"
 
 resource "验证码api" do
   post "/api/v1/validation_codes" do
-
     parameter :email, type: :string
     let(:email) { '1@qq.com' }
-   
     example "请求发送验证码" do
+      expect(UserMailer).to receive(:welcome_email)
       do_request
       expect(status).to eq 200
       expect(response_body).to eq ' '
