a900c7772ddde81ba609c5d3c42ee9a08508912f
曹珂俭 <1849201815@qq.com>
2023-03-30 15:50:27 +0800

    实现登录接口

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    if Rails.env.test?
+      return render status: :unauthorized if params[:code] != '123456'
+    else
+      canSignin = ValidationCode.exists? email: params[:email],code: params[:code], used_at: nil
+      render status: :unauthorized unless canSignin
+    end
+    user = User.find_by_email params[:email]
+    if user.nil?
+      render status: :not_found,json: {errors:'用户不存在'}
+    else
+      render status: :ok,json: {
+        jwt: 'xxxxxxxx'
+      }
+    end
+  end
+end
