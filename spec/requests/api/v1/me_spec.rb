003b94f669a08861ebd04278c61aac8f98a96ac0
曹珂俭 <1849201815@qq.com>
2023-03-30 17:16:10 +0800

    添加autojwt

---
+++ spec/requests/api/v1/me_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe "Me", type: :request do
+  describe "获取当前用户" do
+    it "登录后成功获取" do
+      user = User.create email: '1849201815@qq.com'
+      post '/api/v1/session', params: {email: '1849201815@qq.com', code: '123456'}
+      json = JSON.parse response.body
+      jwt = json['jwt']
+
+      get '/api/v1/me', headers: {'Authorization': "Bearer #{jwt}"}
+      expect(response).to have_http_status(200)
+      json = JSON.parse response.body
+      expect(json['resource']['id']).to eq user.id
+    end
+  end
+end
