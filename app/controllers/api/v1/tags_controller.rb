9250b36adcefa5b6b75f71df4f9b948bb2b9f283
曹珂俭 <1849201815@qq.com>
2023-03-31 18:12:48 +0800

    完成获取单个标签的接口

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -10,6 +10,12 @@ class Api::V1::TagsController < ApplicationController
     } }
   end
 
+  def show
+    tag = Tag.find params[:id]
+    return head :forbidden unless tag.user_id == request.env['current_user_id']
+    render json:{resource: tag}
+  end
+
   def create
     current_user = User.find request.env["current_user_id"]
     return render status: 401 if current_user.nil?
@@ -34,7 +40,7 @@ class Api::V1::TagsController < ApplicationController
 
   def destroy
     tag = Tag.find params[:id]
-    if not tag.user_id == request.env['current_user_id']
+    if not tag.user_id == request.env["current_user_id"]
       return render status: :forbidden
     end
     tag.delete_at = Time.now
