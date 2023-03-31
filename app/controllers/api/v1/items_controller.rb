2b2f1fcda12f962b1457759549e83a639c7532e7
曹珂俭 <1849201815@qq.com>
2023-03-31 20:17:12 +0800

    完善api文档

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,33 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return head :unauthorized if current_user_id.nil?
-    items = Item.where({user_id: current_user_id}).where({created_at: params[:created_after]..params[:created_before]}).page(params[:page])
+    items = Item.where({ user_id: current_user_id }).where({ created_at: params[:created_after]..params[:created_before] }).page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
-      count: Item.count
-    }}
+      count: Item.count,
+    } }
   end
 
   def create
-    item = Item.new params.permit(:amount,:happen_at,tags_id:[])
-    item.user_id = request.env['current_user_id']
+    item = Item.new params.permit(:amount, :happen_at, tags_id: [])
+    item.user_id = request.env["current_user_id"]
     if item.save
       render json: { resource: item }
     else
-      render json: {errors: item.errors},status: :unprocessable_entity
+      render json: { errors: item.errors }, status: :unprocessable_entity
     end
   end
+
   def summary
     hash = Hash.new
     items = Item
-      .where(user_id: request.env['current_user_id'])
+      .where(user_id: request.env["current_user_id"])
       .where(kind: params[:kind])
       .where(happen_at: params[:happened_after]..params[:happened_before])
     items.each do |item|
-      if params[:group_by] == 'happen_at'
-        key = item.happen_at.in_time_zone('Beijing').strftime('%F')
+      if params[:group_by] == "happen_at"
+        key = item.happen_at.in_time_zone("Beijing").strftime("%F")
         hash[key] ||= 0
         hash[key] += item.amount
       else
@@ -37,22 +38,17 @@ class Api::V1::ItemsController < ApplicationController
           hash[key] += item.amount
         end
       end
-      key = item.happen_at.in_time_zone('Beijing').strftime('%F')
-      hash[key] ||= 0
-      hash[key] += item.amount
     end
     groups = hash
-    .map { |key, value| {"#{params[:group_by]}": key, amount: value} }
-    if params[:group_by] == 'happen_at'
+      .map { |key, value| { "#{params[:group_by]}": key, amount: value } }
+    if params[:group_by] == "happen_at"
       groups.sort! { |a, b| a[:happen_at] <=> b[:happen_at] }
-    elsif params[:group_by] == 'tag_id'
+    elsif params[:group_by] == "tag_id"
       groups.sort! { |a, b| b[:amount] <=> a[:amount] }
     end
-      .map { |key, value| {"happen_at": key, amount: value} }
-      .sort { |a, b| a[:happen_at] <=> b[:happen_at] }
     render json: {
       groups: groups,
-      total: items.sum(:amount)
+      total: items.sum(:amount),
     }
   end
 end
