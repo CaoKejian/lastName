33ee8c04faddabb05ffa60213fe60fc971d8f846
曹珂俭 <1849201815@qq.com>
2023-03-31 16:32:42 +0800

    可以直接用email和code登录

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe "Sessions", type: :request do
       json = JSON.parse response.body
       expect(json['jwt']).to be_a(String)
     end
+    it '首次登录' do
+      post '/api/v1/session', params: {email: '1849201815@qq.com',code:'123456'}
+      expect(response).to have_http_status(200)
+      json = JSON.parse response.body
+      expect(json['jwt']).to be_a(String)
+    end
   end
 end
